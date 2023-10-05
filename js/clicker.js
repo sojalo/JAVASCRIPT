20f8449c92121b3ca4557eee5298a2ea83d73cbb
Olajos_Mihaly <olajos.mihaly78@gmail.com>
2023-10-05 22:31:06 +0200

    #10 nap vége - Eseménykezelés

--- js/clicker.js
+++ js/clicker.js
@@ -1,10 +1,102 @@
 let clickingAreaNode = document.querySelector(".js-clicking-area-container");
+// let clickingAreaLeft = document.querySelector(".js-skills-tbody");
+let inventoryContainerNode = document.querySelector(".js-inventory-container");
 
 // Állapottér
-let seconds = 0;
-let bitcoin = 0;
-let bitcoinPerClick = 1;
-let bitcoinPerSec = 0;
+let {seconds,  bitcoin,  bitcoinPerClick,  bitcoinPerSec, skillList, employeeList} = getInitialState();
+
+function getInitialState(){  
+  return {
+    seconds: 0,
+    bitcoin: 0,
+    bitcoinPerClick: 1,
+    bitcoinPerSec: 0,
+    skillList : [
+      {
+        skillName: 'Bitcoin kutatás',
+        bitcoinPerIncrement: 1,
+        description: 'Ahol a bányászatot megváltoztatják, bitcoint találhatunk.',
+        amount: 0,
+        price: 10,
+        link: "./assets/bitcoin_kutatas.png",
+      },
+      {
+        skillName: 'Bróker képzés',
+        bitcoinPerIncrement: 10,
+        description: 'Bányászok betanítását készpénzre válthatjuk.',
+        amount: 0,
+        price: 200,
+        link: "./assets/broker_kepzes.png",
+      },
+      {
+        skillName: 'Manager képzés',
+        bitcoinPerIncrement: 25,
+        description: 'Minél jobban kitanuljuk a managerek képzésének tudományát, annál több managert tudunk értékesíteni bitcoinért cserébe.',
+        amount: 0,
+        price: 750,
+        link: "./assets/manager_kepzes.png",
+      },
+      {
+        skillName: 'Kereskedelem',
+        bitcoinPerIncrement: 100,
+        description: 'BitCoinok értékesítésével profitot termelhetünk.',
+        amount: 0,
+        price: 4000,
+        link: "./assets/kereskedelem.png",
+      },
+      {
+        skillName: 'Tőzsde',
+        bitcoinPerIncrement: 300,
+        description: 'A bányászat hatását tovább erősíti és magasabb kereskedelmi tevékenységet is végezhetünk.',
+        amount: 0,
+        price: 15000,
+        link: "./assets/tozsde.png",
+      },
+    ],
+    employeeList : [
+      {
+        employeeName: 'BitCoin kutató',
+        bitcoinPerSecIncrement: 1,
+        description: 'Bitcoint keres és talál.',
+        amount: 0,
+        price: 100,
+        link: "./assets/bitcoin_kutato.png",
+      },
+      {
+        employeeName: 'Bróker képző',
+        bitcoinPerSecIncrement: 5,
+        description: 'Szerződéses munkatársként bányászokat tanít.',
+        amount: 0,
+        price: 1000,
+        link: "./assets/broker_kepzo.png",
+      },
+      {
+        employeeName: 'Manager képző',
+        bitcoinPerSecIncrement: 10,
+        description: 'Managereket képez ki és értékesít a piacon.',
+        amount: 0,
+        price: 3000,
+        link: "./assets/manager_kepzo.png",
+      },
+      {
+        employeeName: 'Kereskedő',
+        bitcoinPerSecIncrement: 25,
+        description: 'Blokkláncokat készít és értékesít.',
+        amount: 0,
+        price: 10000,
+        link: "./assets/kereskedo.png",
+      },
+      {
+        employeeName: 'Befektető Warren Buffett',
+        bitcoinPerSecIncrement: 100,
+        description: 'Kezeli és fialtatja a vagyonodat.',
+        amount: 0,
+        price: 50000,
+        link: "./assets/befekteto.png",
+      },
+    ],
+  };
+}
 
 function getClickingAreaTemplate(){
   return `
@@ -20,10 +112,25 @@ function getClickingAreaTemplate(){
   `;
 }
 
+/************** click event listener **********************************************************/
 function handleBitcoinClicked(event) {
   if(event.target.dataset.enable_click === "true") {
     bitcoin += bitcoinPerClick;
-    // clickingAreaNode.innerHTML = getClickingAreaTemplate();
+    render();
+  }
+}
+
+function handleInventoryClicked(event){
+  let clickIndex = event.target.dataset.index;
+  if (typeof clickIndex !== "undefined") {
+    let clickedSkill = skillList[clickIndex];
+    if(bitcoin < clickedSkill.price){
+      alert("Nem áll rendelkezésedre elég bitcoin!");
+      return;
+    }
+    bitcoin -= clickedSkill.price;
+    bitcoinPerClick += clickedSkill.bitcoinPerIncrement;
+    clickedSkill.amount += 1;
     render();
   }
 }
@@ -37,50 +144,7 @@ function formatPrice(price) {
 }
 
 /********************************************************************************************/ 
-let skillList = [
-  {
-    skillName: 'Bitcoin kutatás',
-    bitcoinPerIncrement: 1,
-    description: 'Ahol a bányászatot megváltoztatják, bitcoint találhatunk.',
-    amount: 0,
-    price: 10,
-    link: "./assets/bitcoin_kutatas.png",
-  },
-  {
-    skillName: 'Bróker képzés',
-    bitcoinPerIncrement: 10,
-    description: 'Bányászok betanítását készpénzre válthatjuk.',
-    amount: 0,
-    price: 200,
-    link: "./assets/broker_kepzes.png",
-  },
-  {
-    skillName: 'Manager képzés',
-    bitcoinPerIncrement: 25,
-    description: 'Minél jobban kitanuljuk a managerek képzésének tudományát, annál több managert tudunk értékesíteni bitcoinért cserébe.',
-    amount: 0,
-    price: 750,
-    link: "./assets/manager_kepzes.png",
-  },
-  {
-    skillName: 'Kereskedelem',
-    bitcoinPerIncrement: 100,
-    description: 'BitCoinok értékesítésével profitot termelhetünk.',
-    amount: 0,
-    price: 4000,
-    link: "./assets/kereskedelem.png",
-  },
-  {
-    skillName: 'Tőzsde',
-    bitcoinPerIncrement: 300,
-    description: 'A bányászat hatását tovább erősíti és magasabb kereskedelmi tevékenységet is végezhetünk.',
-    amount: 0,
-    price: 15000,
-    link: "./assets/tozsde.png",
-  },
-];
-
-function getSkill({skillName, bitcoinPerIncrement, description, amount, price, link}){
+function getSkill({skillName, bitcoinPerIncrement, description, amount, price, link}, index){
   return `
     <tr>
       <td class="upgrade-text-cell">      
@@ -92,71 +156,17 @@ function getSkill({skillName, bitcoinPerIncrement, description, amount, price, l
         <p>ár: ${formatPrice(price)}</p>
       </td>
       <td class="upgrade-icon-cell">
-        <img class="skill-image" src="${link}" alt="${skillName}">
+        <img 
+          class="skill-image" 
+          src="${link}" alt="${skillName}" 
+          data-index="${index}" />
       </td>
     </tr>
   `;
 }
 
-// 1. megoldás
-// let rows = [];
-// for(let skill of skillList){
-//   rows.push(getSkill(skill));
-// }
-// document.querySelector(".js-skills-tbody").innerHTML = rows.join("");
-  
-// 2. megoldás
-// let skillHtml ="";
-// for(let skill of skillList){
-//   skillHtml += getSkill(skill);
-// }
-// document.querySelector(".js-skills-tbody").innerHTML = skillHtml;
-
-// 3. megoldás Map
-
 /********************************************************************************************/ 
-let employeeList = [
-  {
-    employeeName: 'BitCoin kutató',
-    bitcoinPerSecIncrement: 1,
-    description: 'Bitcoint keres és talál.',
-    amount: 0,
-    price: 100,
-    link: "./assets/bitcoin_kutato.png",
-  },
-  {
-    employeeName: 'Bróker képző',
-    bitcoinPerSecIncrement: 5,
-    description: 'Szerződéses munkatársként bányászokat tanít.',
-    amount: 0,
-    price: 1000,
-    link: "./assets/broker_kepzo.png",
-  },
-  {
-    employeeName: 'Manager képző',
-    bitcoinPerSecIncrement: 10,
-    description: 'Managereket képez ki és értékesít a piacon.',
-    amount: 0,
-    price: 3000,
-    link: "./assets/manager_kepzo.png",
-  },
-  {
-    employeeName: 'Kereskedő',
-    bitcoinPerSecIncrement: 25,
-    description: 'Blokkláncokat készít és értékesít.',
-    amount: 0,
-    price: 10000,
-    link: "./assets/kereskedo.png",
-  },
-  {
-    employeeName: 'Befektető Warren Buffett',
-    bitcoinPerSecIncrement: 100,
-    description: 'Kezeli és fialtatja a vagyonodat.',
-    amount: 0,
-    price: 50000,
-    link: "./assets/befekteto.png",
-  },
-];
+
 
 function getEmployee({employeeName, bitcoinPerSecIncrement, description, amount, price, link}){
   return `
@@ -183,22 +193,16 @@ function render() {
 }
 
 function initialize(){
-  bitcoin = 0;
-  seconds = 0;
-  bitcoinPerClick = 1;
-  bitcoinPerSec = 0;
+  let data = getInitialState();
+  seconds = data.seconds;
+  bitcoin = data.bitcoin;
+  bitcoinPerClick = data.bitcoinPerClick;
+  bitcoinPerSec = data.bitcoinPerSec;
   
   clickingAreaNode.addEventListener('click', handleBitcoinClicked);
+  // clickingAreaLeft.addEventListener('click', handleLeftClicked);
+  inventoryContainerNode.addEventListener('click', handleInventoryClicked);
   render();
 }
 
 initialize();
-
-// let businessRows = [];
-// for(let employee of employeeList){
-//   businessRows.push(getEmployee(employee));
-// }
-
-// document.querySelector(".js-business-tbody").innerHTML = businessRows.join("");
-// let businessTemplate = getEmployee(employeeList[0]);
-// document.querySelector(".js-business-tbody").innerHTML = businessTemplate;
